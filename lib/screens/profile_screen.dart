06ed49967b2b8c01c7d0d26cfae597a8822a74c0
dilekhashakthi <dilekhashakthi00@gmail.com>
2026-08-23 00:27:17 +0530

    Create the profile screen and Add image assets and update asset manager for profile and bag features

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,15 +1,170 @@
+import 'package:e_commerce_app/providers/theme_provider.dart';
+import 'package:e_commerce_app/services/assets_manager.dart';
+import 'package:e_commerce_app/widgests/subtitle_text.dart';
 import 'package:e_commerce_app/widgests/title_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: TitleTextWidget(label: "Profile Screen", fontSize: 30),
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(AssetsManager.shoppingCart),
+        ),
+        title: Text("Profile Screen"),
       ),
+      body: Column(
+        children: [
+          Visibility(
+            visible: false,
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: TitleTextWidget(
+                label: "Please login to have unlimited access",
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Visibility(
+            visible: true,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5.0,
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).cardColor,
+                      border: Border.all(color: Colors.lightBlue, width: 3.0),
+                    ),
+                    child: ClipOval(
+                      child: Image.network(
+                        "https://i.pravatar.cc/150?img=12",
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(
+                            Icons.person,
+                            size: 38,
+                            color: Theme.of(context).colorScheme.primary,
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TitleTextWidget(label: "Dilekha Shakthi", fontSize: 18),
+                      SizedBox(height: 6),
+                      SubtitleTextWidget(label: "user@example.com"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Divider(thickness: 1),
+                SizedBox(height: 10),
+                TitleTextWidget(label: "General", fontSize: 18),
+                SizedBox(height: 10),
+                CustomListTile(
+                  text: "All Orders",
+                  imagePath: AssetsManager.orderSvg,
+                  function: () {},
+                ),
+                CustomListTile(
+                  text: "Wishlist",
+                  imagePath: AssetsManager.wishlistSvg,
+                  function: () {},
+                ),
+                CustomListTile(
+                  text: "Viewed Recently",
+                  imagePath: AssetsManager.recent,
+                  function: () {},
+                ),
+                CustomListTile(
+                  text: "Address",
+                  imagePath: AssetsManager.address,
+                  function: () {},
+                ),
+                SizedBox(height: 10),
+                Divider(thickness: 1),
+                SizedBox(height: 10),
+                TitleTextWidget(label: "Settings", fontSize: 18),
+                SizedBox(height: 10),
+                SwitchListTile(
+                  secondary: Image.asset(AssetsManager.theme, height: 34),
+                  title: Text(
+                    themeProvider.getIsDarkTheme ? "Dark Theme" : "Light Theme",
+                  ),
+                  value: themeProvider.getIsDarkTheme,
+                  onChanged: (value) {
+                    themeProvider.setDarkTheme(themeValue: value);
+                  },
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              onPressed: () {},
+              icon: Icon(IconlyLight.login),
+              label: Text("Login"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  const CustomListTile({
+    super.key,
+    required this.text,
+    required this.imagePath,
+    required this.function,
+  });
+
+  final String text, imagePath;
+  final Function function;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        function();
+      },
+      title: Text(text),
+      leading: Image.asset(imagePath, height: 34),
+      trailing: Icon(IconlyLight.arrowRight2),
     );
   }
 }
---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,46 @@
+class AssetsManager {
+  static String imagePath = "assets/images";
+  static String profileImagePath = "assets/images/profile";
+  static String bagImagePath = "assets/images/bag";
+  static String bannerImagePath = "assets/images/banners";
+  static String categoryImagePath = "assets/images/categories";
+
+  // General Images
+  static String forgetPassword = "$imagePath/forget_password.png";
+  static String mapRounded = "$imagePath/rounded_map.png";
+  static String warning = "$imagePath/warning.png";
+  static String error = "$imagePath/error.png";
+  static String addressMap = "$imagePath/address_map.png";
+  static String emptySearch = "$imagePath/empty_search.png";
+  static String successful = "$imagePath/successful.png";
+
+  // Profile Images
+  static String address = "$profileImagePath/address.png";
+  static String login = "$profileImagePath/login.png";
+  static String logout = "$profileImagePath/logout.png";
+  static String privacy = "$profileImagePath/privacy.png";
+  static String recent = "$profileImagePath/recent.png";
+  static String theme = "$profileImagePath/theme.png";
+
+  // Bag Images
+  static String bagWish = "$bagImagePath/bag_wish.png";
+  static String shoppingBasket = "$bagImagePath/shopping_basket.png";
+  static String shoppingCart = "$bagImagePath/shopping_cart.png";
+  static String orderBag = "$bagImagePath/order.png";
+  static String orderSvg = "$bagImagePath/order_svg.png";
+  static String wishlistSvg = "$bagImagePath/wishlist_svg.png";
+
+  // Banner Images
+  static String banner1 = "$bannerImagePath/banner1.png";
+  static String banner2 = "$bannerImagePath/banner2.png";
+
+  // Category Images
+  static String mobiles = "$categoryImagePath/mobiles.png";
+  static String fashion = "$categoryImagePath/fashion.png";
+  static String watch = "$categoryImagePath/watch.png";
+  static String book = "$categoryImagePath/book_img.png";
+  static String electronics = "$categoryImagePath/electronics.png";
+  static String cosmetics = "$categoryImagePath/cosmetics.png";
+  static String shoes = "$categoryImagePath/shoes.png";
+  static String pc = "$categoryImagePath/pc.png";
+}
